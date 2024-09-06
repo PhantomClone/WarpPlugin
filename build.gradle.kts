e6808f177591202a235b3cf39c5b582183d0f5a1
PhantomClone <fabian.mueller2000@gmx.de>
2024-09-06 14:38:18 +0200

    update gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,16 @@ import dev.s7a.gradle.minecraft.server.tasks.LaunchMinecraftServerTask
 
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.0"
     id("dev.s7a.gradle.minecraft.server") version "3.0.0"
 }
 
 group = "me.phantomclone.warpplugin"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
 
 repositories {
     mavenCentral()
@@ -18,14 +22,25 @@ repositories {
 
 dependencies {
     implementation("de.chojo.sadu", "sadu", "1.3.1")
-    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.postgresql:postgresql:42.7.2")
     implementation("org.reflections:reflections:0.10.2")
     implementation("org.flywaydb:flyway-core:9.22.3")
 
-    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 
-    compileOnly("org.projectlombok:lombok:1.16.10")
-    annotationProcessor("org.projectlombok:lombok:1.18.20")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
+    testImplementation("org.mockito:mockito-core:5.13.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.13.0")
+    testImplementation("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(21)
+    options.encoding = "UTF-8"
 }
 
 tasks {
@@ -42,10 +57,17 @@ tasks {
         archiveFileName.set("${project.name}-${project.version}.jar")
         //minimize() TODO optimize
     }
+}
 
-    compileJava {
-        options.release.set(17)
-        options.encoding = "UTF-8"
+
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+
+    maxHeapSize = "1G"
+
+    testLogging {
+        events("passed")
     }
 }
 
@@ -54,11 +76,11 @@ task<LaunchMinecraftServerTask>("testPlugin") {
 
     doFirst {
         copy {
-            from(buildDir.resolve("libs/${project.name}-${project.version}.jar"))
-            into(buildDir.resolve("MinecraftServer/plugins"))
+            from(layout.buildDirectory.dir("libs/${project.name}-${project.version}.jar"))
+            into(layout.buildDirectory.dir("MinecraftServer/plugins"))
         }
     }
 
-    jarUrl.set(LaunchMinecraftServerTask.JarUrl.Paper("1.20.2"))
+    jarUrl.set(LaunchMinecraftServerTask.JarUrl.Paper("1.21.1"))
     agreeEula.set(true)
 }
